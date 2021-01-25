a3bc8fbdbde98b73bf528c04275e0f421cacdfb5
Usman45268 <43941568+Usman45268@users.noreply.github.com>
2021-01-26 00:35:24 +0800

    Update detail.php

--- detail.php
+++ detail.php
@@ -57,7 +57,5 @@
       <p>Support by : Freepik, Bootstrap
   </footer>
  </div>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script>window.jQuery || document.write('<script src="../assets/js/vendor/jquery.slim.min.js"><\/script>')</script><script src="js/bootstrap.bundle.min.js"></script>
 
 </body>
